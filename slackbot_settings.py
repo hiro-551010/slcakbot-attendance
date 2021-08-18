0ed9c8692ab1a93b1534f944835b187c3652a678
hiro-551010 <hiro551010@gmail.com>
2021-08-18 20:57:08 +0900

    修正

---
+++ slackbot_settings.py
@@ -0,0 +1,12 @@
+import os
+import yaml
+
+with open(os.path.join('secrets', 'secret.yaml')) as file:
+    objs = yaml.safe_load(file)
+    for obj in objs:
+        os.environ[obj] = objs[obj]
+
+
+    API_TOKEN = os.environ['API_TOKEN']
+
+    DEFAULT_REPLY = "こんにちは、こちらは勤怠管理botです。"
