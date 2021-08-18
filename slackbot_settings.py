81a1e5bf005f582e56623d8e4a87a1ccda2bacfd
hiro-551010 <hiro551010@gmail.com>
2021-08-19 06:22:52 +0900

    修正

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,15 +1,19 @@
 import os
 import yaml
 
-with open(os.path.join('secrets', 'secret.yaml')) as file:
-    objs = yaml.safe_load(file)
-    for obj in objs:
-        os.environ[obj] = objs[obj]
+try:
+    with open(os.path.join('secrets', 'secret.yaml')) as file:
+        objs = yaml.safe_load(file)
+        for obj in objs:
+            os.environ[obj] = objs[obj]
 
 
-    API_TOKEN = os.environ['API_TOKEN']
+        API_TOKEN = os.environ['API_TOKEN']
 
-    DEFAULT_REPLY = "こんにちは、こちらは勤怠管理botです。"
+        DEFAULT_REPLY = "こんにちは、こちらは勤怠管理botです。"
+
+except:
+    pass
 
 # herokuのAPI_TOKENの設定
 API_TOKEN = os.environ['API_TOKEN']
