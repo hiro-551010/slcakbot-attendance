736efce7cf560c78bd3ea5fb9b3b92bd65ed7ad7
hiro-551010 <hiro551010@gmail.com>
2021-08-26 22:40:54 +0900

    環境変数設定

--- gsd.py
+++ gsd.py
@@ -16,16 +16,16 @@ class Auth():
     
     # herokuから環境変数として取得
     CREDENTIAL = {
-        "type": os.environ['type'],
-        "project_id": os.environ['project_id'],
-        "private_key_id": os.environ['private_key_id'],
-        "private_key": os.environ['private_key'],
-        "client_email": os.environ['client_email'],
-        "client_id": os.environ['client_id'],
-        "auth_uri": os.environ['auth_uri'],
-        "token_uri": os.environ['token_uri'],
-        "auth_provider_x509_cert_url": os.environ['auth_provider_x509_cert_url'],
-        "client_x509_cert_url": os.environ['client_x509_cert_url'],
+        "type": "service_account",
+        "project_id": os.environ['PROJECT_ID'],
+        "private_key_id": os.environ['PRIVATE_KEY_ID'],
+        "private_key": os.environ['PRIVATE_KEY'].replace('\\n', '\n'),
+        "client_email": os.environ['CLIENT_EMAIL'],
+        "client_id": os.environ['CLIENT_ID'],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ['SHEET_CLIENT_X509_CERT_URL']
     }
 
     def __init__(self):
